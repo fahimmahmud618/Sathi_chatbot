79e7976e3098c9eca235d508dde0d23fbea38e18
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2024-03-06 11:47:44 +0600

    home page decoration done

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.css";
+import chatpic from "./asset/chat.png"
+import servicepic from "./asset/service.png"
 export default function Home() {
   return (
     <div className="bodyhome">
@@ -9,105 +11,79 @@ export default function Home() {
       </div>
       <div className="d2">
         <center>
-          <p className="hometitle pt-3 pb-3">
-            <u>গুরুত্বপুর্ণ নাগরিক সেবা</u>
+          <div className="first_section">
+            <p className="hometitle pt-3 pb-3">
+          গুরুত্বপুর্ণ নাগরিক সেবা<br />
+            <img className="logoicon1" src={servicepic} alt="Chat" />
+
           </p>
-          <div className="row p-3 d-flex justify-content-around">
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://pcc.police.gov.bd/ords/pcc2/r/pcc/home"
-                >
-                  পুলিশ ক্লিয়ারেন্স
-                </a>
-              </li>
-            </div>
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://mowca.gov.bd/site/forms/df4ad3fe-8faa-48e2-a039-f246a44f01f7/%E0%A6%AC%E0%A6%BF%E0%A6%A7%E0%A6%AC%E0%A6%BE-%E0%A6%93-%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%BE%E0%A6%AE%E0%A7%80-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%A4%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%95%E0%A7%8D%E0%A6%A4-%E0%A6%AD%E0%A6%BE%E0%A6%A4%E0%A6%BE-%E0%A6%86%E0%A6%AC%E0%A7%87%E0%A6%A6%E0%A6%A8-%E0%A6%AB%E0%A6%B0%E0%A6%AE-"
-                >
-                  বয়স্ক এবং বিধবা ভাতা
-                </a>
-              </li>
-            </div>
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://www.epassport.gov.bd/"
-                >
-                  পাসপোর্ট
-                </a>
-              </li>
-            </div>
+
+          <div className="col">
+            <a
+              className="aa"
+              target="blank"
+              href="https://pcc.police.gov.bd/ords/pcc2/r/pcc/home"
+            >
+              পুলিশ ক্লিয়ারেন্স
+            </a>
+
+            <a
+              className="aa"
+              target="blank"
+              href="https://mowca.gov.bd/site/forms/df4ad3fe-8faa-48e2-a039-f246a44f01f7/%E0%A6%AC%E0%A6%BF%E0%A6%A7%E0%A6%AC%E0%A6%BE-%E0%A6%93-%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%BE%E0%A6%AE%E0%A7%80-%E0%A6%AA%E0%A6%B0%E0%A6%BF%E0%A6%A4%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%95%E0%A7%8D%E0%A6%A4-%E0%A6%AD%E0%A6%BE%E0%A6%A4%E0%A6%BE-%E0%A6%86%E0%A6%AC%E0%A7%87%E0%A6%A6%E0%A6%A8-%E0%A6%AB%E0%A6%B0%E0%A6%AE-"
+            >
+              বয়স্ক এবং বিধবা ভাতা
+            </a>
+
+            <a
+              className="aa"
+              target="blank"
+              href="https://www.epassport.gov.bd/"
+            >
+              পাসপোর্ট
+            </a>
+
+            <a
+              className="aa"
+              target="blank"
+              href="https://bdris.gov.bd/br/application"
+            >
+              নাগরিক সনদ
+            </a>
+
+            <a
+              className="aa"
+              target="blank"
+              href="https://bdris.gov.bd/br/application"
+            >
+              জন্ম নিবন্ধন
+            </a>
+
+            <a className="aa" target="blank" href="https://www.nidw.gov.bd/">
+              জাতীয় পরিচয়পত্র
+            </a>
           </div>
-          <div className="row p-3 d-flex justify-content-around">
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://bdris.gov.bd/br/application"
-                >
-                  নাগরিক সনদ
-                </a>
-              </li>
-            </div>
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://bdris.gov.bd/br/application"
-                >
-                  জন্ম নিবন্ধন
-                </a>
-              </li>
-            </div>
-            <div className="col">
-              {" "}
-              <li className="w-50 h-100">
-                <a
-                  className="aa"
-                  target="blank"
-                  href="https://www.nidw.gov.bd/"
-                >
-                  জাতীয় পরিচয়পত্র
-                </a>
-              </li>
-            </div>
           </div>
-          <hr />
-          <a href="/chat">
-            <div className="gotochat rounded mt-5 d-flex justify-content-start text-white p-3">
-              <div className="col p-5">
-                <div className="row">
-                  <h2>Introducing </h2>
-                  <b>Sathi Chatbot || Go to Chat</b>
-                </div>
-              </div>
-              <div class="d-flex" style={{ height: "150px" }}>
-                <div class="vr"></div>
-              </div>
-              <div className="col p-5">
-                <b>
+          
+          <div className="second_section">
+            <a href="/chat">
+            <div className="aaa gotochat">
+              
+                <div className="row d-flex justify-content-center ">
+                  <p className="hometitle">সাথী এ-আই চ্যাট <br />
+                  <img className="logoicon1" src={chatpic} alt="Chat" />
+
+                  </p>
+                  <p>
                   Chat with Sathi AI Assistance. Ask for any query to get
                   Government data in your door when you wish...
-                </b>
+                </p>
+                </div>
               </div>
-            </div>
+       
           </a>
+          </div>
+          
         </center>
       </div>
     </div>
